4c8e0d89deec79ca006e52b2d02d3c5f6509f0fc
Series Chikanbanjar <series7.sc@gmail.com>
2021-07-08 19:47:17 +0545

    Play Sound Using Input Text

--- index.js
+++ index.js
@@ -1,72 +1,104 @@
+// Get div by id
 const btnCapture = document.querySelectorAll("#sound");
-console.log(btnCapture);
+const stopBtn = document.querySelector("#stop");
 
+// initialize variable
 let sound;
 let playState = false;
 let btnName;
-// var btnCaptureText;
 
+// set sound play
 function soundPlay(audioPath, name) {
     if (!playState) {
-        sound = new Audio(audioPath);
-        playState = true;
-        sound.play();
+        playSound(audioPath);
     } else {
-        console.log(btnName);
-        console.log(name);
+        // console.log("start and play part");
+        // soundPause();
+        // playSound(audioPath);
         if (btnName == name) {
             console.log("stop part");
             soundPause();
         } else {
             console.log("start and play part");
             soundPause();
-            sound = new Audio(audioPath);
-            playState = true;
-            sound.play();
+            playSound(audioPath);
         }
+    }
+    btnName=name;
+}
+
+// play sound
+function playSound(audioPath){
+    sound = new Audio(audioPath);
+    playState = true;
+    sound.play();
+}
+
+// Pause played sound
+function soundPause() {
+    if(sound !== undefined){
+        sound.pause();
+        playState = false;
+        sound.currentTime = 0;
     }
 }
 
-function soundPause() {
-    sound.pause();
-    playState = false;
-    sound.currentTime = 0;
-}
+// get textInput field
+let soundText = document.querySelector("#textInput");
 
+// get pressed or entered character
 const setBtn = (btnCaptureText) => {
-    console.log(btnCaptureText);
-    console.log(btnCaptureText);
-
     switch (btnCaptureText) {
         case "a": {
             console.log("case 1");
-            btnName = "a";
+            // btnName = "a";
+            console.log(btnName);
+            document.querySelector('#error').innerHTML = "";
             soundPlay("test1.mp3", "a");
             break;
         }
         case "b": {
-            btnName = "b";
+            // btnName = "b";
+            console.log(btnName);
+            document.querySelector('#error').innerHTML = "";
             soundPlay("test2.mp3", "b");
             break;
         }
         case "c": {
-            btnName = "c";
+            // btnName = "c";
+            console.log(btnName);
+            document.querySelector('#error').innerHTML = "";
             soundPlay("test3.mp3", "c");
             break;
         }
         default:
             console.log("default");
+            document.querySelector('#error').innerHTML = "<div class='text-danger'>!!! Invalid Input. Please Enter Valid Input !!!</div>";
+            soundPause();
             break;
     }
 };
 
+// for loop for looping all the data
 for (i = 0; i < btnCapture.length; i++) {
-    // console.log(i);
     const btnCapture = document.querySelectorAll("#sound")[i];
     const btnCaptureText = btnCapture.innerHTML;
-    // console.log(btnCaptureText);
     btnCapture.addEventListener("click", () => {
-        // console.log(btnCaptureText, "im");
         setBtn(btnCaptureText);
     });
 }
+
+
+// Stop Audio
+stopBtn.addEventListener("click", () => {
+    soundPause();
+})
+
+// Play sound using Input field
+soundText.addEventListener('input', function textSound(){
+    console.log(soundText.value);
+    setBtn(soundText.value);
+    if(soundText !== ""){
+        document.querySelector('#textInput').value = "";
+    }    
+});
